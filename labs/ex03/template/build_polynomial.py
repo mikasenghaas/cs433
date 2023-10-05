da3560fd3f8626a8b64db8b543d21215f55fa9f8
Mika Senghaas <mail@mikasenghaas.de>
2023-10-05 18:01:01 +0200

    feat(ex03): added solution to week 3 exercises

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -6,10 +6,10 @@ import numpy as np
 
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # polynomial basis function: TODO
-    # this function should return the matrix formed
-    # by applying the polynomial basis to the input data
-    # ***************************************************
-    raise NotImplementedError
+
+    # Create augmented features by iterating over degrees
+    augmented_features = np.hstack(
+        [(x**d).reshape(-1, 1) for d in range(0, degree + 1)]
+    )
+
+    return augmented_features
