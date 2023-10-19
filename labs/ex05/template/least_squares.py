52cdaf98f5ba6227a9774aadbda392a1e195cba0
Mika Senghaas <mail@mikasenghaas.de>
2023-10-19 16:59:33 +0200

    feat(ex05): solved exercise 5

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -9,9 +9,4 @@ import numpy as np
 
 def least_squares(y, tx):
     """calculate the least squares."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-    raise NotImplementedError
+    return np.linalg.solve(tx.T @ tx, tx.T @ y)
